df56dc66d66cb829a0faca41278301be600f3753
黄志敏 <huangzhimin@zohetec.com>
2025-04-02 15:27:11 +0800

    add core::start()

--- src/core.cpp
+++ src/core.cpp
@@ -1,6 +1,7 @@
 #include "config_factory.h"
 #include "core.h"
 #include "error.h"
+#include "helper.h"
 #include <string>
 
 namespace jgb
@@ -55,6 +56,9 @@ int core::install(const char* name, jgb_app_t* app)
     app_conf_->add(name, conf);
     //jgb_debug("{ name = %s, conf = %p }", name, conf);
 
+    struct ap ap_x {name, app, conf};
+    ap_.push_back(ap_x);
+
     if(app)
     {
         if(app->version != current_app_interface_version)
@@ -93,11 +97,32 @@ struct ap* core::find(const char* name)
     return nullptr;
 }
 
-// TODO:
 int core::start(const char* path)
 {
-    path = path;
-    return 0;
+    std::string base;
+    int r;
+    const char* s = path;
+    const char* e;
+
+    r = jgb::jpath_parse(&s, &e);
+    if(!r)
+    {
+        std::string name(s, e);
+        struct ap* ap;
+        ap = find(name.c_str());
+        if(ap)
+        {
+            if(ap->app
+                    && ap->app->task)
+            {
+                // TODO
+                jgb_info("start task. { path = \"%s\", app = %s }", path, name.c_str());
+                return 0;
+            }
+        }
+    }
+    jgb_fail("start task. { path = \"%s\" }", path);
+    return JGB_ERR_FAIL;
 }
 
 }
