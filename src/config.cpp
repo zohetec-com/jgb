f33df6c53e3e17e960718ca09bc2c36db2564428
黄志敏 <huangzhimin@zohetec.com>
2025-08-05 13:09:16 +0800

    修改调试打印

--- src/config.cpp
+++ src/config.cpp
@@ -259,7 +259,7 @@ int value::get(const char* path, value** val, int* idx)
             }
             else
             {
-                jgb_debug("{ s = %s, idx = %d }", s, idx);
+                jgb_debug("not found. { s = %s }", s);
                 return JGB_ERR_NOT_FOUND;
             }
         }
